19612d98ae5a24ad739060b4067d7c6af25e9b85
David Caba <ct55609@gmail.com>
2022-10-26 08:38:23 -0400

    version inicial del proyecto

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GameComponent from "./components/memory/GameComponent";
+
+ReactDOM.render(<GameComponent />, document.getElementById("root"));
